b446cbb8aca17245ea73557174127e811923337c
HiloniB <54409505+HiloniB@users.noreply.github.com>
2024-04-18 14:35:38 -0700

    first

---
+++ homepage.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from PIL import Image
+
+page_icon = Image.open("./favicon.ico")
+st.set_page_config(page_title="Famiology", page_icon=page_icon, layout="wide", initial_sidebar_state="expanded")
+logo = Image.open("./favicon.ico")
+
+# Streamlit UI
+st.sidebar.image("FamiologyTextLogo.png", use_column_width=True)
+
+st.markdown(
+    """
+    <style>
+        section[data-testid="stSidebar"] {
+            width: 400px !important; # Set the width to your desired value
+        }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+def main():
+    with st.sidebar.container():
+        # Section 1: Apps
+        with st.expander("Apps"):
+            st.markdown("Document Detector")
+            st.markdown('<a href="https://famiology-smart-fill.streamlit.app/" target="_self">Smart Fill</a>', unsafe_allow_html=True)
+        # Section 2: Dashboards
+        with st.expander("Dashboards"):
+            st.markdown("Dashboard")
+
+if __name__ == "__main__":
+    main()
