57f7f6186a9209810ee40630c1ba0aa75a887441
Hiloni20 <158073489+Hiloni20@users.noreply.github.com>
2024-05-08 11:23:07 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 import streamlit as st
 from PIL import Image
 
-page_icon = Image.open("pages/favicon.ico")
+page_icon = Image.open("./favicon.ico")
 st.set_page_config(page_title="Famiology", page_icon=page_icon, layout="wide", initial_sidebar_state="expanded")
-logo = Image.open("pages/favicon.ico")
+logo = Image.open("./favicon.ico")
 
 # Streamlit UI
-st.sidebar.image("FamiologyTextLogo.png", use_column_width=True)
+# st.sidebar.image("FamiologyTextLogo.png", use_column_width=True)
 
 # st.sidebar.expander("Apps")
 
 # st.sidebar.expander("Dashboards")
 
 # st.sidebar.selectbox('Apps', options=['Document Detector', 'Smart Fill'], index=0)
-# st.sidebar.selectbox('Dashboards', options=['Dashboard'], index=1)
+#st.sidebar.selectbox('Dashboards', options=['Dashboard'], index=1)
 
 st.markdown(
     """
@@ -32,21 +32,11 @@ def main():
     with st.sidebar.container():
         # Section 1: Apps
         with st.expander("Apps"):
-            st.markdown('<a href="https://famiologydocdetector.streamlit.app/" target="_self">Document Detector</a>',unsafe_allow_html=True)
+            st.markdown('<a href="https://famiologydocdetector.streamlit.app/" target="_self">Document Detector</a>', unsafe_allow_html=True)
             st.markdown('<a href="https://famiology-smart-fill.streamlit.app/" target="_self">Smart Fill</a>', unsafe_allow_html=True)
-        # Section 2: Dashboards
-        with st.expander("Dashboards"):
-            st.markdown("Dashboard")
+        # # Section 2: Dashboards
+        # with st.expander("Dashboards"):
+        #     st.markdown("Dashboard")
 
 if __name__ == "__main__":
     main()
-
-
-# def main():
-#     with st.sidebar.container():
-#         st.markdown('<a href="https://famiologydocdetector.streamlit.app/" target="_self">Document Detector</a>',unsafe_allow_html=True)
-
-# if __name__ == "__main__":
-#     main()
-        
-    
