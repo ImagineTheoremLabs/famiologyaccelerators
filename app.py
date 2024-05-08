fec83965d9f56e3096f0f3a976572220f588d53c
Hiloni20 <158073489+Hiloni20@users.noreply.github.com>
2024-05-08 10:59:52 -0700

    Update app.py

--- app.py
+++ app.py
@@ -42,11 +42,11 @@ st.markdown(
 #     main()
 
 
-def main():
-    with st.sidebar.container():
-        st.markdown('<a href="https://famiologydocdetector.streamlit.app/" target="_self">Document Detector</a>',unsafe_allow_html=True)
+# def main():
+#     with st.sidebar.container():
+#         st.markdown('<a href="https://famiologydocdetector.streamlit.app/" target="_self">Document Detector</a>',unsafe_allow_html=True)
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
         
     
