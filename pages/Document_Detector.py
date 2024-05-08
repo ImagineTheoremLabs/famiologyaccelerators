c20937568ea907fda69af546abe97d3546ec76b4
Hiloni20 <158073489+Hiloni20@users.noreply.github.com>
2024-05-08 11:03:21 -0700

    Update Document_Detector.py

--- pages/Document_Detector.py
+++ pages/Document_Detector.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 def main():
-    with st.sidebar.container():
-        st.markdown('<a href="https://famiologydocdetector.streamlit.app/" target="_self">Document Detector</a>',unsafe_allow_html=True)
+    with st.sidebar():
+        st.markdown('<a href="https://famiologydocdetector.streamlit.app/" target="_self">Document Detector</a>', unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
