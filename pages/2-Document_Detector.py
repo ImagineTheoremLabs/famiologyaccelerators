25c5d9d3f4658aa8535cefe3c3377ae58d533cfa
tgupta222 <111917056+tgupta222@users.noreply.github.com>
2024-06-12 14:57:30 -0400

    Update 2-Document_Detector.py

--- pages/2-Document_Detector.py
+++ pages/2-Document_Detector.py
@@ -278,17 +278,17 @@ def main():
     # st.sidebar.image("FamiologyTextLogo.png", use_column_width=True)
 
     file = open("FamiologyTextLogo.png", "rb")
-contents = file.read()
-img_str = base64.b64encode(contents).decode("utf-8")
-buffer = io.BytesIO()
-file.close()
-img_data = base64.b64decode(img_str)
-img = Image.open(io.BytesIO(img_data))
-resized_img = img.resize((400, 100))  # x, y
-resized_img.save(buffer, format="PNG")
-img_b64 = base64.b64encode(buffer.getvalue()).decode("utf-8")
+    contents = file.read()
+    img_str = base64.b64encode(contents).decode("utf-8")
+    buffer = io.BytesIO()
+    file.close()
+    img_data = base64.b64decode(img_str)
+    img = Image.open(io.BytesIO(img_data))
+    resized_img = img.resize((400, 100))  # x, y
+    resized_img.save(buffer, format="PNG")
+    img_b64 = base64.b64encode(buffer.getvalue()).decode("utf-8")
 
-st.markdown(
+    st.markdown(
         f"""
         <style>
             [data-testid="stSidebarNav"] {{
