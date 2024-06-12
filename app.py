a11686da792d30340961b85ea09ca600ae181711
tgupta222 <111917056+tgupta222@users.noreply.github.com>
2024-06-12 13:20:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 from PIL import Image
 
-page_icon = Image.open("favicon.ico")
+page_icon = Image.open("pages/favicon.ico")
 st.set_page_config(page_title="Famiology", page_icon=page_icon, layout="wide", initial_sidebar_state="expanded")
 logo = Image.open("pages/favicon.ico")
 
-Streamlit UI
+# Streamlit UI
 st.sidebar.image("FamiologyTextLogo.png", use_column_width=True)
 
 st.markdown(
@@ -19,13 +19,17 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
-def main():
-    with st.sidebar.container():
-        # Section 1: Apps
-        with st.expander("Apps"):
-            st.markdown('<a href="https://famiologydocdetector.streamlit.app/" target="_self">Document Detector</a>', unsafe_allow_html=True)
-            st.markdown('<a href="https://famiology-smart-fill.streamlit.app/" target="_self">Smart Fill</a>', unsafe_allow_html=True)
+# def main():
+#     with st.sidebar.container():
+#         # Section 1: Apps
+#         with st.expander("Apps"):
+#             st.markdown('<a href="https://famiologydocdetector.streamlit.app/" target="_self">Document Detector</a>', unsafe_allow_html=True)
+#             st.markdown('<a href="https://famiology-smart-fill.streamlit.app/" target="_self">Smart Fill</a>', unsafe_allow_html=True)
+        
+
+# if __name__ == "__main__":
+#     main()
 
 
-if __name__ == "__main__":
-    main()
+
+
