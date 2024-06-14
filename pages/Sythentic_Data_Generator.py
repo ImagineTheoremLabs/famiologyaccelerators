cb0f028da1a626639576436dba4a54dc986a5fcc
tgupta222 <111917056+tgupta222@users.noreply.github.com>
2024-06-14 13:22:18 -0400

    Update Sythentic_Data_Generator.py

--- pages/Sythentic_Data_Generator.py
+++ pages/Sythentic_Data_Generator.py
@@ -330,6 +330,33 @@ import math
 from datetime import datetime, timedelta
 from io import BytesIO
 
+#setting Famiology Logo at top of sidebar
+file = open("FamiologyTextLogo.png", "rb")
+contents = file.read()
+img_str = base64.b64encode(contents).decode("utf-8")
+buffer = io.BytesIO()
+file.close()
+img_data = base64.b64decode(img_str)
+img = Image.open(io.BytesIO(img_data))
+resized_img = img.resize((375, 75))  # x, y
+resized_img.save(buffer, format="PNG")
+img_b64 = base64.b64encode(buffer.getvalue()).decode("utf-8")
+st.markdown(
+        f"""
+        <style>
+            [data-testid="stSidebarNav"] {{
+                background-image: url('data:image/png;base64,{img_b64}');
+                background-repeat: no-repeat;
+                padding-top: 120px;
+                background-position: 20px 20px;
+            }}
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+
+
+
 class CustomEmailProvider(BaseProvider):
     def email(self, first_name, last_name, domain="example.com"):
         return f"{first_name.lower()}.{last_name.lower()}@{domain}"
