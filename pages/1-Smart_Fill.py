de0a8e1e0f4bd3e0bb48f80f426b26e7db2553d1
tgupta222 <111917056+tgupta222@users.noreply.github.com>
2024-06-14 15:14:05 -0400

    Update 1-Smart_Fill.py

--- pages/1-Smart_Fill.py
+++ pages/1-Smart_Fill.py
@@ -73,7 +73,7 @@ st.markdown(
     """
     <style>
         section[data-testid="stSidebar"] {
-            width: 340px !important; # Set the width to your desired value
+            width: 338px !important; # Set the width to your desired value
         }
     </style>
     """,
