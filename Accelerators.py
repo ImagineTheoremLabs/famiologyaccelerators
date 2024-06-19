48946d0db3405cc0757a56c418ff15d916af3195
tgupta222 <tgupta6@gmu.edu>
2024-06-18 23:41:10 -0700

    update

--- Accelerators.py
+++ Accelerators.py
@@ -12,13 +12,27 @@ import pandas as pd
 from datetime import datetime
 import base64
 
+# logo on tab
 page_icon = Image.open("pages/favicon.ico")
 st.set_page_config(page_title="Famiology", page_icon=page_icon, layout="wide", initial_sidebar_state="expanded")
 logo = Image.open("pages/favicon.ico")
 
-# Streamlit UI
+# set width of sidebar
+st.markdown(
+    """
+    <style>
+        section[data-testid="stSidebar"] {
+            width: 338px !important; # Set the width to your desired value
+        }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+# logo on sidebar
 # st.sidebar.image("FamiologyTextLogo.png", use_column_width=True)
 
+# logo specifically on top of sidebar
 file = open("FamiologyTextLogo.png", "rb")
 contents = file.read()
 img_str = base64.b64encode(contents).decode("utf-8")
@@ -29,7 +43,6 @@ img = Image.open(io.BytesIO(img_data))
 resized_img = img.resize((300, 60))  # x, y
 resized_img.save(buffer, format="PNG")
 img_b64 = base64.b64encode(buffer.getvalue()).decode("utf-8")
-
 st.markdown(
         f"""
         <style>
@@ -44,17 +57,44 @@ st.markdown(
         unsafe_allow_html=True,
     )
 
+# HTML and CSS to center the logo and title
+html = """
+<div style="text-align: center;">
+    <img src="FamiologyTextLogo.png" alt="Logo" width="700">
+    <h1>Famiology Accelerators</h1>
+</div>
+"""
 
-st.markdown(
-    """
-    <style>
-        section[data-testid="stSidebar"] {
-            width: 338px !important; # Set the width to your desired value
-        }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
+# Display the HTML
+st.markdown(html, unsafe_allow_html=True)
+
+# Add a subheading
+st.subheader("Welcome to Famiology, the ultimate Life Management Solution for Generation GenX. ")
+
+# Add some introductory text
+st.write("""
+Our platform offers unparalleled convenience by streamlining financial and life document management. Simplify your life and stay organized effortlessly with Famiology.""")
+
+# Add another subheading
+st.subheader("Features")
+
+# Add some text under the second subheading
+st.write("""
+Here are some of the features of Streamlit:
+- **Simplicity**: Create apps with just a few lines of code.
+- **Speed**: Fast development and deployment.
+- **Interactivity**: Easily add interactivity to your apps.
+- **Customization**: Customize the look and feel of your apps.
+""")
+
+# Add another subheading
+st.subheader("Getting Started")
+
+# Add some text under the third subheading
+st.write("""
+To get started with Streamlit, install it using pip and create a new Python script. You can use various Streamlit commands to add elements to your app, such as titles, text, charts, and more.
+""")
+
 
 # def main():
 #     with st.sidebar.container():
@@ -66,7 +106,3 @@ st.markdown(
 
 # if __name__ == "__main__":
 #     main()
-
-
-
-
