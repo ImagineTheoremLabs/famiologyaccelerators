ef8aa1dcb7e10659d0dcd78172c69a917ebfe1f0
Atharva Bapat <65668471+Atharva7775@users.noreply.github.com>
2024-07-06 17:28:18 -0500

    Update Famiology Compute Engine.py

--- pages/Famiology Compute Engine.py
+++ pages/Famiology Compute Engine.py
@@ -90,7 +90,9 @@ def main():
         <div class='fixed-demo'>
             <h2 id='working-demo'>Working Demo</h2>
             <video controls width='100%'>
-            <source src='img/Demo-Video.mp4'>
+            video_file = open(‘’, ‘rb’)
+            video_bytes = video_file.read()    
+            st.video(video_bytes)
                 Your browser does not support the video tag.
             </video>
         </div>
