8d2ad40dbca37e9e8ca12a4afb1773e167ed8cc5
Atharva Bapat <65668471+Atharva7775@users.noreply.github.com>
2024-07-06 16:53:13 -0500

    Update and rename page.py to Famiology Compute Engine.py

--- pages/Famiology Compute Engine.py
+++ pages/Famiology Compute Engine.py
@@ -92,7 +92,7 @@ def main():
         <div class='fixed-demo'>
             <h2 id='working-demo'>Working Demo</h2>
             <video controls width='100%'>
-                <source src='path_to_your_video.mp4' type='video/mp4'>
+                <source src='img/Demo-Video.mp4' type='video/mp4'>
                 Your browser does not support the video tag.
             </video>
         </div>
